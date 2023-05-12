a879a4d8f042307df655e315806fb33945520624
Chasan Ali Mujahid <chasanalim07@gmail.com>
2023-05-12 08:42:09 +0700

    FIX - Klik Kanan Kamar Inap

--- src/simrskhanza/DlgKamarInap.java
+++ src/simrskhanza/DlgKamarInap.java
@@ -5984,21 +5984,21 @@ public class DlgKamarInap extends javax.swing.JDialog {
             }
             
             if(evt.getClickCount()==1){
-                i=tbKamIn.getSelectedColumn();
-                    if(i==2){
-                        if(validasicatatan.equals("Yes")){
-                            this.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-                            LabelCatatan.setText(Sequel.cariIsi("select catatan_pasien.catatan from catatan_pasien where catatan_pasien.no_rkm_medis=?",TNoRMCari.getText()));
-                            if(!LabelCatatan.getText().equals("")){
-                                DlgCatatan.setLocationRelativeTo(Scroll);
-                                DlgCatatan.setVisible(true);
-                            }else{
-                                DlgCatatan.setLocationRelativeTo(Scroll);
-                                DlgCatatan.setVisible(true);
-                            }                            
-                            this.setCursor(Cursor.getDefaultCursor());
-                        }  
-                    }      
+//                i=tbKamIn.getSelectedColumn();
+//                    if(i==2){
+//                        if(validasicatatan.equals("Yes")){
+//                            this.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+//                            LabelCatatan.setText(Sequel.cariIsi("select catatan_pasien.catatan from catatan_pasien where catatan_pasien.no_rkm_medis=?",TNoRMCari.getText()));
+//                            if(!LabelCatatan.getText().equals("")){
+//                                DlgCatatan.setLocationRelativeTo(Scroll);
+//                                DlgCatatan.setVisible(true);
+//                            }else{
+//                                DlgCatatan.setLocationRelativeTo(Scroll);
+//                                DlgCatatan.setVisible(true);
+//                            }                            
+//                            this.setCursor(Cursor.getDefaultCursor());
+//                        }  
+//                    }      
             }
             if(evt.getClickCount()==2){
                 i=tbKamIn.getSelectedColumn();
