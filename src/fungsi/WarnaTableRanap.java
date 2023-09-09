e67095fd895acfdb5dcaeaa8ce5b3e3c15f9ea2e
Chasan Ali Mujahid <chasanalim07@gmail.com>
2023-09-09 09:36:38 +0700

    Fix - Warna List Rawat Inap Pasien Titipan

--- src/fungsi/WarnaTableRanap.java
+++ src/fungsi/WarnaTableRanap.java
@@ -16,8 +16,8 @@ import javax.swing.table.DefaultTableCellRenderer;
  */
 public class WarnaTableRanap extends DefaultTableCellRenderer {
     public int kolom;
-    public int jnsbayar= 6;
-    public int statbayar = 20;
+    public int jnsbayar= 6,statbayar = 20;
+
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column){
         Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
@@ -25,25 +25,36 @@ public class WarnaTableRanap extends DefaultTableCellRenderer {
             component.setBackground(new Color(255,255,255));
         }else{
             component.setBackground(new Color(255,255,255));
-        } 
+        }
+        
         if (table.getValueAt(row, jnsbayar).toString().equals("BPJS KESEHATAN")){
             component.setBackground(new Color(160, 230, 180));
-//            component.setForeground(new Color(255,255,255));
+            
         }
+        
+//        if (table.getValueAt(row, 23).toString().equals("Kelas 2")){
+//            component.setBackground(new Color(255,200,100));
+//            
+//        }
+        
+        
         if (table.getValueAt(row, jnsbayar).toString().contains("(Asuransi)")){
             component.setBackground(new Color(224, 252, 130));
-         
-//            component.setForeground(new Color(255,255,255));
         }
         
         if (table.getValueAt(row, 22).toString().contains(".")){
             component.setBackground(new Color(255,153,204));
-//            component.setForeground(new Color(255,255,255));
         }
+        
+        if (table.getValueAt(row, 23).toString().contains("Kelas")){
+            component.setBackground(new Color(255,200,100));
+        }
+        
+        
+        
         
         if (table.getValueAt(row, statbayar).toString().equals("Sudah Bayar")){
             component.setBackground(new Color(129, 217, 252));
-//            component.setForeground(new Color(255,255,255));
         }
         
         
