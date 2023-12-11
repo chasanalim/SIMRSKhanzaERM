176c9f6718dae586845c11825a2dc24ab3e68a0f
Chasan Ali Mujahid <chasanalim07@gmail.com>
2023-12-11 09:30:38 +0700

    Fix - Permintaan Resep

--- src/inventory/DlgDaftarPermintaanResep.java
+++ src/inventory/DlgDaftarPermintaanResep.java
@@ -2211,7 +2211,7 @@ private void KdKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_TKdKey
                         Sequel.queryu("delete from antriapotek3");
                         Sequel.queryu("insert into antriapotek3 values('"+NoResep+"','1','"+NoRawat+"')");
                         Sequel.queryu("delete from bukti_penyerahan_resep_obat where no_resep='"+NoResep+"'");
-//                        Sequel.mengedit("resep_obat","'"+TNoRw.getText()+"','Sudah Diterima','0000-00-00 00:00:00',now(),'0000-00-00 00:00:00','0000-00-00 00:00:00','0000-00-00 00:00:00'","status='Sudah Diterima',diterima=now()","no_rawat='"+TNoRw.getText()+"'");
+                        Sequel.mengedit("resep_obat","no_resep='"+NoResep+"'","tgl_penyerahan=current_date(),jam_penyerahan=current_time()");
                     }
                 }else{
                     JOptionPane.showMessageDialog(null,"Maaf, Anda tidak punya hak akses untuk mengvalidasi...!!!!");
